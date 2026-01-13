86dd8b47c9a1dc15b070ca044bea84dc74408ac5
Bongani Moche <bonganimoche8@gmail.com>
2026-01-13 11:30:50 +0200

    improvde routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Topbar from './components/Topbar';
-import Dashboard from './features/DashboardSummary/pages/Dashboard';
-import Onboarding from './features/SchoolOnboarding/pages/Onboarding';
-import Schools from './features/ListOfOnboardedSchools/pages/Schools';
-// import Settings from './pages/Settings';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRouter from './routes/AppRouter'; // your router with AdminLayout
 
 export default function App() {
   return (
     <Router>
-      <div className="flex min-h-screen bg-[#0f172a]">
-        <Sidebar />
-        
-        <div className="flex-1">
-          <Topbar />
-          
-          <main className="pt-16 md:pl-64">
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/onboarding" element={<Onboarding />} />
-              <Route path="/schools" element={<Schools />} />
-              {/* <Route path="/settings" element={<Settings />} /> */}
-              <Route path="*" element={<Dashboard />} /> {/* default */}
-            </Routes>
-          </main>
-        </div>
-      </div>
+      <AppRouter />
     </Router>
   );
 }
